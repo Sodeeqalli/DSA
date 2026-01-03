6e8053a1b14d40073081e3b11c6a50f7db20b5f7
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-01-02 22:51:47 -0700

    Time: 82 ms (23.94%) | Memory: 22.7 MB (25.24%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self,key= -1,value= -1):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hashMap = [ListNode() for i in range(10**4)]
+        
+
+    def put(self, key: int, value: int) -> None:
+        index = key % 10**4
+        curr = self.hashMap[index]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.value = value
+                return
+            curr = curr.next
+        curr.next = ListNode(key,value)
+        
+
+    def get(self, key: int) -> int:
+        index = key % 10**4
+        curr = self.hashMap[index]
+        while curr.next:
+            if curr.next.key == key:
+                return curr.next.value
+            curr = curr.next
+        return -1 
+        
+
+    def remove(self, key: int) -> None:
+        index = key % 10**4
+        curr = self.hashMap[index]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+         
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
