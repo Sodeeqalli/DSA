ea38312e8982456be55cead9681d4e18dedc8b8c
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-02-11 14:11:39 -0700

    Time: 3 ms (68.31%) | Memory: 19.4 MB (49.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,44 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l3 = ListNode()
-
+        curr1, curr2 = l1, l2
         carry = 0
-        curr1, curr2, curr3 = l1,l2,l3
-        while curr1 and curr2:
-            addition = carry + curr1.val + curr2.val
-            carry = 0
-            if addition >= 10:
-                carry += 1
-                addition -= 10
-            curr3.next = ListNode(addition)
-            curr3 = curr3.next
-            curr1 = curr1.next
-            curr2 = curr2.next
-        
-        while curr1:
-            addition = carry + curr1.val
-            carry = 0
-            if addition >= 10:
-                carry+=1
-                addition-=10
-            curr3.next = ListNode(addition)
-            curr3 = curr3.next
-            curr1 = curr1.next
 
-        while curr2:
-            addition = carry + curr2.val
-            carry = 0
-            if addition >= 10:
-                carry+=1
-                addition-=10
-            curr3.next = ListNode(addition)
-            curr3 = curr3.next
-            curr2 = curr2.next
-        
-        if carry > 0:
-            curr3.next = ListNode(carry)
-        
-        return l3.next
+        dummy = ListNode()
+        curr3 = dummy
 
+        while curr1 or curr2 or carry:
+            value = (curr1.val if curr1 else 0) + (curr2.val if curr2 else 0) + carry
+            curr3.next = ListNode(val = value % 10)
+            carry = value // 10
+            curr1 = curr1.next if curr1 else None
+            curr2 = curr2.next if curr2 else None
+            curr3 = curr3.next
+
+        return dummy.next
         
+
+
+
+
