cd3f86f150a9898b86c3653f7019e68daa1741a1
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-03-08 16:25:23 -0600

    Time: 173 ms (30.59%) | Memory: 79.3 MB (19.43%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,24 +1,22 @@
-#implementing listNode
 class ListNode:
-    def __init__(self, value = -1, prev = None, nxt = None):
+    def __init__(self,value = -1,prev = None,nxt = None):
         self.value = value
         self.prev = prev
         self.next = nxt
 
-#implementing Doubly Linked List
 class LinkedList:
     def __init__(self):
         self.left = ListNode()
         self.right = ListNode()
         self.left.next = self.right
         self.right.prev = self.left
-        self.map = {}
-
+        self.map = {} #key to the node
+    
     def length(self):
         return len(self.map)
-    
+
     def pushRight(self,value):
-        node = ListNode(value, self.right.prev, self.right)
+        node = ListNode(value,self.right.prev,self.right)
         node.prev.next = node
         self.right.prev = node
         self.map[value] = node
@@ -33,57 +31,62 @@ class LinkedList:
             self.map.pop(value, None)
     
     def popLeft(self):
-        if self.length() > 0:
-            v = self.left.next.value
-            self.pop(v)
-            return v
-        return None
-        
+        if len(self.map) > 0:
+            value = self.left.next.value
+            self.pop(value)
+            return value
+        return 
+
 class LFUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.valMap = {} #key to value
         self.lfuCount = 0
+        self.valMap = {}
         self.freqMap = defaultdict(int)
         self.listMap = defaultdict(LinkedList)
-    
-    def counter(self, key):
+
+    def counter(self,key):
         cnt = self.freqMap[key]
         self.freqMap[key] += 1
 
         if cnt > 0:
             self.listMap[cnt].pop(key)
-        self.listMap[cnt+1].pushRight(key)
+        self.listMap[cnt + 1].pushRight(key)
 
-        if self.lfuCount == cnt and self.listMap[self.lfuCount].length() == 0:
+        if cnt == self.lfuCount and self.listMap[cnt].length() == 0:
             self.lfuCount+=1
-    
+
     def get(self, key: int) -> int:
         if key not in self.valMap:
             return -1
         self.counter(key)
         return self.valMap[key]
+        
 
     def put(self, key: int, value: int) -> None:
         if self.cap == 0:
             return
         
         if key in self.valMap:
-            self.counter(key)
             self.valMap[key] = value
+            self.counter(key)
             return
         
         if len(self.valMap) == self.cap:
-           evict = self.listMap[self.lfuCount].popLeft()
-           self.valMap.pop(evict, None)
-           self.freqMap.pop(evict, None) 
+            evict = self.listMap[self.lfuCount].popLeft()
+            self.valMap.pop(evict,None)
+            self.freqMap.pop(evict, None)
         
         self.valMap[key] = value
         self.freqMap[key] = 1
         self.listMap[1].pushRight(key)
         self.lfuCount = 1
 
-        
 
 
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
