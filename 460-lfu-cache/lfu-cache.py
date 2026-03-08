ea3e1075c9a8f0983bbba9dc0f2d455c34c3d42a
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-03-08 00:50:48 -0700

    Time: 167 ms (34.95%) | Memory: 79.3 MB (13.20%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,25 +1,24 @@
-#implementing a listNode
+#implementing listNode
 class ListNode:
-    def __init__(self,value = -1,prev = -1,nxt = None):
+    def __init__(self, value = -1, prev = None, nxt = None):
         self.value = value
         self.prev = prev
         self.next = nxt
 
-#implementing a doubly linked list
-
+#implementing Doubly Linked List
 class LinkedList:
     def __init__(self):
         self.left = ListNode()
         self.right = ListNode()
-        self.map = {}
         self.left.next = self.right
         self.right.prev = self.left
+        self.map = {}
 
     def length(self):
         return len(self.map)
     
     def pushRight(self,value):
-        node = ListNode(value,self.right.prev,self.right)
+        node = ListNode(value, self.right.prev, self.right)
         node.prev.next = node
         self.right.prev = node
         self.map[value] = node
@@ -35,29 +34,29 @@ class LinkedList:
     
     def popLeft(self):
         if self.length() > 0:
-            value = self.left.next.value
-            self.pop(value)
-            return value
+            v = self.left.next.value
+            self.pop(v)
+            return v
         return None
-
+        
 class LFUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity                         #capacity
-        self.lfuCount = 0                           #the least frequent count
-        self.valMap = {}                            #map of keys to values
-        self.freqMap = defaultdict(int)             #frequency of keys
-        self.ListMap = defaultdict(LinkedList)      #Linkedlist of each frequency
+        self.cap = capacity
+        self.valMap = {} #key to value
+        self.lfuCount = 0
+        self.freqMap = defaultdict(int)
+        self.listMap = defaultdict(LinkedList)
     
     def counter(self, key):
-        #current freq of the key
-        cFreq = self.freqMap[key]
-        #increase by one
+        cnt = self.freqMap[key]
         self.freqMap[key] += 1
-        if cFreq > 0:
-            self.ListMap[cFreq].pop(key)
-        self.ListMap[cFreq+1].pushRight(key)
-        if cFreq == self.lfuCount and self.ListMap[self.lfuCount].length() == 0:
+
+        if cnt > 0:
+            self.listMap[cnt].pop(key)
+        self.listMap[cnt+1].pushRight(key)
+
+        if self.lfuCount == cnt and self.listMap[self.lfuCount].length() == 0:
             self.lfuCount+=1
     
     def get(self, key: int) -> int:
@@ -65,32 +64,26 @@ class LFUCache:
             return -1
         self.counter(key)
         return self.valMap[key]
-        
 
     def put(self, key: int, value: int) -> None:
         if self.cap == 0:
             return
         
         if key in self.valMap:
-            self.valMap[key] = value
             self.counter(key)
+            self.valMap[key] = value
             return
-
+        
         if len(self.valMap) == self.cap:
-            evict = self.ListMap[self.lfuCount].popLeft()
-            self.valMap.pop(evict, None)
-            self.freqMap.pop(evict,None)
+           evict = self.listMap[self.lfuCount].popLeft()
+           self.valMap.pop(evict, None)
+           self.freqMap.pop(evict, None) 
         
         self.valMap[key] = value
-        self.ListMap[1].pushRight(key)
         self.freqMap[key] = 1
+        self.listMap[1].pushRight(key)
         self.lfuCount = 1
 
+        
 
 
-
-
-# Your LFUCache object will be instantiated and called as such:
-# obj = LFUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
