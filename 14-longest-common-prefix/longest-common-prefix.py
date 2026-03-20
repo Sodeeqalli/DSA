2214299fa9e85865d0cd8d6c0a4124b5e726a3b5
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-03-20 16:01:36 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.66%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = ""
+
         for i in range(len(strs[0])):
             for j in range(1,len(strs)):
                 if i == len(strs[j]) or strs[0][i] != strs[j][i]:
@@ -8,6 +9,8 @@ class Solution:
             prefix += strs[0][i]
         
         return prefix
+
+
 
 
         
