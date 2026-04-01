d88185fbc2262699dba1c7cf1670f5a1d26566a6
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-04-01 16:14:43 -0600

    Time: 59 ms (6.52%) | Memory: 23.7 MB (5.80%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,17 +1,25 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        seen = defaultdict(int)
-        seen[0] = 1
-        prefixSum = 0
-        count = 0
+        #[1,1,1]
+        #[0,1,2,3]
 
-        for num in nums:
-            prefixSum += num
-            if prefixSum - k in seen:
-                count += seen[prefixSum - k]
-            seen[prefixSum] += 1
+        #[1,2,3]
+        #[0,1,3,6]
         
-        return count
-            
-            
+        prefixSum = [0] * (len(nums)+1)
+        sumMap = defaultdict(int)
+        subArrayCount = 0
 
+        for i in range(len(nums)):
+            prefixSum[i+1] = prefixSum[i] + nums[i]
+        
+        for i in range(len(prefixSum)):
+            complement = prefixSum[i] - k
+            subArrayCount += sumMap[complement]
+            sumMap[prefixSum[i]] += 1
+
+        return subArrayCount
+
+
+        
+        
