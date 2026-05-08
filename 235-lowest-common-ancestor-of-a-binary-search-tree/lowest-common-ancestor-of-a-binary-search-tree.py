0d673a1755a9a21fd833662324653d25fbc68e8d
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-05-08 16:57:08 -0600

    Time: 65 ms (55.97%) | Memory: 22.8 MB (17.66%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,51 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pAncestors = {}
-        qAncestors = {}
-        
-        #search for p
-        curr = root
-        level = 1
-        while curr:
-            pAncestors[level] = curr
-            if curr.val == p.val:
-                curr = None
-            elif curr.val > p.val:
-                curr = curr.left
-            else:
-                curr = curr.right
-            level+=1
-        
-        curr = root
-        level = 1
-        while curr:
-            qAncestors[level] = curr
-            if curr.val == q.val:
-                curr = None
-            elif curr.val > q.val:
-                curr = curr.left
-            else:
-                curr = curr.right
-            level +=1
-
-        while len(pAncestors) > len(qAncestors):
-            pAncestors.pop(max(pAncestors.keys()), None)
-        
-        while len(qAncestors) > len(pAncestors):
-            qAncestors.pop(max(qAncestors.keys()), None)
-        
-        while qAncestors[max(qAncestors.keys())] != pAncestors[max(pAncestors.keys())]:
-            qAncestors.pop(max(qAncestors.keys()), None)
-            pAncestors.pop(max(pAncestors.keys()), None)
-        
-        return qAncestors.pop(max(qAncestors.keys()), None)
-        
-        
-
-        
-
-            
-            
+        if p == root or q == root:
+            return root
+        if p.val > root.val and q.val < root.val or p.val < root.val and q.val > root.val:
+            return root
+        if p.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return self.lowestCommonAncestor(root.left, p, q)
 
         
