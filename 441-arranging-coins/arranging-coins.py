a5386cfbcfc056d3d4b94865fde44c2f65487acd
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-05-13 15:32:36 -0600

    Time: 2 ms (75.99%) | Memory: 19.3 MB (31.00%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,10 +1,27 @@
 class Solution:    
+    def numOfCoins(self, Row):
+        return (Row/2)*(Row+1)
+
     def arrangeCoins(self, n: int) -> int:
-        stairCount = 0
-        rowLength = 1
-        while n >= rowLength:
-            n-=rowLength
-            stairCount += 1
-            rowLength += 1
-        return stairCount
+        l,r = 1, n
+        res = 1
+
+        while l <= r:
+            m = l + (r-l)//2
+            numCoins = self.numOfCoins(m)
+            if numCoins > n:
+                r = m - 1
+            elif numCoins < n:
+                res = m
+                l = m + 1
+            else:
+                return m
+        
+        return res
+
+                
+
+            
+
+        
         
