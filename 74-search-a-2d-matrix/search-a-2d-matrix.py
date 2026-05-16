55a9c41c3fb943fc09a006f6fe472f5e5c99ab75
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-05-15 21:10:37 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (97.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,34 +1,46 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        col, row = len(matrix), len(matrix[0])
-
-        #binary search to search value range
-        l,r = 0, col - 1
-
-        while l<=r:
-            m = l+((r-l)//2) 
-            lower,upper = matrix[m][0],matrix[m][row-1]
-            if lower > target:
-                r = m - 1
-            elif upper < target:
-                l = m + 1
-            else:
-                break
-        
-        if not (l<=r):
-            return False
-        
-        #binary search to find value in array
-        l,r =0, row-1
+        #search arrays
+        m, n = len(matrix), len(matrix[0])
+        l,r = 0, m-1
+        index = float("-inf")
 
         while l<=r:
             mid = l + (r-l)//2
-            if matrix[m][mid] == target:
-                return True
-            elif matrix[m][mid] > target:
+
+            if target > matrix[mid][n-1]:
+                l = mid + 1
+            elif target < matrix[mid][0]:
                 r = mid - 1
             else:
+                index = mid
+                break
+
+        if index == float("-inf"):
+            return False
+        
+        
+        l,r = 0,n-1
+
+        while l <= r:
+            mid = l + (r-l)//2
+
+            if target > matrix[index][mid]:
                 l = mid + 1
+            elif target < matrix[index][mid]:
+                r = mid - 1
+            else:
+                return True
         
         return False
+
+
+
+
+
+
+
+
+        #search array
+
         
