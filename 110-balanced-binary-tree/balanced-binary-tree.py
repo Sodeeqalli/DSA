af27cdff1ba503d60d2c6fdbc402da05049e993d
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-05-15 20:51:03 -0600

    Time: 11 ms (5.98%) | Memory: 20.8 MB (5.35%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,23 +6,25 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node):
+        #if there is no root, its balanced
+        #if there is no root.left and no root.right, its balance
+        #then we check the depth of left and right, if the difference is greater than one we return False else True
+        def depth(node):
             if not node:
                 return 0
-            
-            left = dfs(node.left)
-            right = dfs(node.right)
+            return 1 + max(depth(node.left), depth(node.right))
 
-            if left == -1 or right == -1:
-                return -1
-            
-            if abs(left-right) > 1:
-                return -1
-            
-            return 1 + max(left, right)
+        if not root:
+            return True
+        if not root.left and not root.right:
+            return True
         
-        return dfs(root) != -1
+        leftHeight, rightHeight = depth(root.left), depth(root.right)
 
+        if abs(leftHeight-rightHeight) > 1:
+            return False
+        
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
 
 
 
