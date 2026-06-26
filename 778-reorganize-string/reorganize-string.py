f19a2555560878be6cc3a9ca932a1d072140fd7e
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-06-25 18:52:24 -0600

    Time: 4 ms (17.48%) | Memory: 19.3 MB (80.64%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,40 +1,24 @@
-from collections import deque, Counter
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        #take the count
-        #2a 2b
-        #then we use a queue to schedule the next place it can be avaialable, then we remove from the queue
-
-        characterCount = Counter(s)
-
-        maxHeap = [(-count,character) for character, count in characterCount.items()]
+        count = Counter(s)
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
         heapq.heapify(maxHeap)
-        q = deque()
-        position = 0
-        string = ""
 
-        while maxHeap or q:
-            
-            position+=1
-
-            if q and q[0][0] == position:
-                _,countLeft, character = q.popleft()
-                heapq.heappush(maxHeap, (countLeft, character))
-
-            if maxHeap:
-                count,character = heapq.heappop(maxHeap)
-                string += character
-                countLeft = count + 1
-                nextPos = position + 2
-                if countLeft < 0:
-                    q.append([nextPos,countLeft,character])
-            else:
+        prev = None
+        res = ""
+        while maxHeap or prev:
+            if prev and not maxHeap:
                 return ""
-            
-           
-            
-        return string
 
-                
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
 
-        
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+
+            if cnt != 0:
+                prev = [cnt, char]
+
+        return res
