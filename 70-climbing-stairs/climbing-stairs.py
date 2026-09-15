6364324de78284cdea8d65e231178e9f26139654
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-15 12:38:51 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.95%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,20 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = [None]*(n+1)
+        memo = [None] * (n+1)
 
         def climb(i):
+            if i == n:
+                return 1
             if i > n:
                 return 0
             if memo[i] != None:
                 return memo[i]
-            if i == n:
-                memo[i] = 1
-                return 1
             
+
             memo[i] = climb(i+1) + climb(i+2)
             return memo[i]
         
-        return climb(0)
-            
 
+        return climb(0)
         
