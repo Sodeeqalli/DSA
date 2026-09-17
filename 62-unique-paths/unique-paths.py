3739fc4873dfc80b8078d23390915b01203caac2
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-16 21:56:55 -0600

    Time: 2 ms (23.71%) | Memory: 19.7 MB (11.79%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[None for _ in range(n)] for _ in range(m)]
+
+        def move(row, col):
+            if row == m or col == n:
+                return 0
+            if row == m -1 and col == n-1:
+                return 1
+            if memo[row][col] != None:
+                return memo[row][col]
+            
+            memo[row][col] = move(row+1, col) + move(row, col+1)
+            
+            return memo[row][col]
+        
+        return move(0,0)
+
+
+        
