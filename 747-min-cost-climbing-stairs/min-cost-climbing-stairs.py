bea9981abd02981554e1e3b3b4b293c2fb0b59bd
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-16 21:39:45 -0600

    Time: 5 ms (20.68%) | Memory: 20.8 MB (16.94%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,27 +1,36 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #from where we are, we need to find cheapest way to get to top
-        #managing running costs from the two separate paths is my problem now
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        #we can start either from index 1 and index 0
+        #so the initial state can be 0 or 1 not fixed (should we run the recursive function twice and find the min)
+        #at every stage we pick 1 or 2, but since we want to return minimun cost, our function should return the min cost of if we take 1 or 2 steps
+        #if we go beyond cost.length, then we return 0, if we are at cost.length we return the cost
+        #welp we cant return 0 that will be the min cost
+        #the problem now is how do we keep account of the costs
+        #i am thinking now that the state is actually cost or like no the function is supposed to return cost
+        
         n = len(cost)
-        memo = [None]*(n+1)
-
+        memo = [None] * n
         def climb(i):
-            if i >= n:
+            if i > n:
+                return float("inf")
+            if i == n:
                 return 0
             if memo[i] != None:
                 return memo[i]
-            
+
             memo[i] = cost[i] + min(climb(i+1), climb(i+2))
-        
+            
             return memo[i]
-
-        startOne = climb(0)
-        memo = [None]*n
-        startTwo = climb(1)
         
-        return min(startOne, startTwo)
+        return min(climb(0), climb(1))
+
+       
+
+            
 
 
 
 
+
+        
         
