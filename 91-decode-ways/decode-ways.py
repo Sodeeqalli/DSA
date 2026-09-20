5c8c51ec969e1314b5454f40ba8608937ed0f02d
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-20 13:00:24 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.84%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,25 +2,20 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         
         n = len(s)
-        dp = [0] * n
+        dp = [0] * (n + 1)
+        dp[n] = 1
         #how many ways can we decode last digit
         if s[n-1] == "0":
             dp[n-1] = 0
         else:
-            dp[n-1] = 1
+            dp[n-1] = dp[n]
 
         if n == 1:
             return dp[0]
         
         #how many ways can we second to last digit
-        if s[n-2] == "0":
-            dp[n-2] = 0
-        elif s[n-2] == "1" or (s[n-2] == "2" and int(s[n-1]) < 7):
-            dp[n-2] = 1 + dp[n-1]
-        else:
-            dp[n-2] = dp[n-1]
 
-        for i in range(n-3, -1, -1):
+        for i in range(n-2, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
             elif s[i] == "1" or (s[i] == "2" and int(s[i+1]) < 7):
