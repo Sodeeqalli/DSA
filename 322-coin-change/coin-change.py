59bc0a9789a23dc74c7457279396c866bb62ebe7
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-22 17:42:05 -0600

    Time: 847 ms (23.32%) | Memory: 31.8 MB (20.47%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -7,31 +7,24 @@ class Solution:
 
         def solve(curValue):
             if curValue > amount:
-                return [-1,False]
+                return float("inf")
             if curValue == amount:
-                return [0,True]
+                return 0
             if memo[curValue] != None:
                 return memo[curValue]
             
             #im struggling with how to keep count of coins taken so far
             #im struggling to model choices
             minimum = float("inf")
-            reached = False
             for i in range(len(coins)):
-                minFromI, reachedFromI = solve(curValue + coins[i])
-                if reachedFromI and minFromI < minimum:
-                    reached = reachedFromI
-                    minimum = minFromI
+                minimum = min(solve(curValue + coins[i]), minimum)
 
-            if reached == False:
-                memo[curValue] = [-1, reached]
-                return memo[curValue]
-
-            memo[curValue] = [1 + minimum, reached]
-
+            memo[curValue] = 1 + minimum
             return memo[curValue]
 
-        return solve(0)[0]
+        fewest = solve(0)
+
+        return fewest if fewest != float("inf") else -1
 
 
 
