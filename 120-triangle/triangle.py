acd51d4bb57debc4f3deffc92a3795020ea9caa2
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-21 21:34:33 -0600

    Time: 0 ms (100.00%) | Memory: 20.7 MB (21.49%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,20 +1,35 @@
 class Solution:
     def minimumTotal(self, triangle: list[list[int]]) -> int:
-        #let me solve with recursion first
-        m, n= len(triangle), len(triangle[-1])
-        memo = [[None for _ in range(n)] for _ in range(m)] 
+        # #let me solve with recursion first
+        # m, n= len(triangle), len(triangle[-1])
+        # memo = [[None for _ in range(n)] for _ in range(m)] 
 
-        def traverse(r,c):
-            if r == m:
-                return 0
-            if memo[r][c] != None:
-                return memo[r][c]
+        # def traverse(r,c):
+        #     if r == m:
+        #         return 0
+        #     if memo[r][c] != None:
+        #         return memo[r][c]
             
-            memo[r][c] = triangle[r][c] + min(traverse(r+1,c), traverse(r+1,c+1))
+        #     memo[r][c] = triangle[r][c] + min(traverse(r+1,c), traverse(r+1,c+1))
 
-            return memo[r][c]
+        #     return memo[r][c]
         
-        return traverse(0,0)
+        # return traverse(0,0)
+
+        m, n = len(triangle), len(triangle[-1])
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        for col in range(n):
+            dp[m-1][col] = triangle[m-1][col]
+        
+        for i in range(m-2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+        
+        return dp[0][0]
+
+
 
 
         
