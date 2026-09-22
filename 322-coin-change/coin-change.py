bf34fef9db22f1f42ef94edb4ae1a34a385dcfc1
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-22 17:57:14 -0600

    Time: 539 ms (44.96%) | Memory: 19.7 MB (53.41%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -2,32 +2,21 @@ class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
         #this seems similar to the combination problem where i have all states still available after picking one
 
-        memo = [None] * (amount+1)
+        dp = [0] * (amount+1)
 
+        dp[amount] = 0
 
-        def solve(curValue):
-            if curValue > amount:
-                return float("inf")
-            if curValue == amount:
-                return 0
-            if memo[curValue] != None:
-                return memo[curValue]
-            
-            #im struggling with how to keep count of coins taken so far
-            #im struggling to model choices
+        for i in range(amount-1, -1, -1):
             minimum = float("inf")
-            for i in range(len(coins)):
-                minimum = min(solve(curValue + coins[i]), minimum)
 
-            memo[curValue] = 1 + minimum
-            return memo[curValue]
-
-        fewest = solve(0)
-
-        return fewest if fewest != float("inf") else -1
-
-
-
+            for coin in coins:
+                if coin + i <= amount:
+                    minimum = min(1 + dp[coin+i], minimum)
+            
+            dp[i] = minimum
+            
+        return dp[0] if dp[0] != float("inf") else -1
+               
                 
              
         
