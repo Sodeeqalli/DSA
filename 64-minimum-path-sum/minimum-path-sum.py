98ae109d7c63dd58cede6e0feef9994824db03e0
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-21 22:12:27 -0600

    Time: 11 ms (56.00%) | Memory: 21.6 MB (49.82%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,25 +1,24 @@
 class Solution:
     def minPathSum(self, grid: list[list[int]]) -> int:
+        #dependence = current box + min(down, right)
+        #bottom up
         m, n = len(grid), len(grid[0])
-        memo = [[None for _ in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        dp[m-1][n-1] = grid[m-1][n-1]
+
+        #fill last col
+        for row in range(m-2, -1, -1):
+            dp[row][n-1] = grid[row][n-1] + dp[row+1][n-1]
+        
+        #fill last row
+        for col in range(n-2, -1, -1):
+            dp[m-1][col] = grid[m-1][col] + dp[m-1][col+1]
 
 
-        def move(row,col):
-            if row == m or col == n:
-                return float("inf")
-            
-            if row == m-1 and col == n-1:
-                return grid[row][col]
-
-            if memo[row][col] != None:
-                return memo[row][col]
-            
-            memo[row][col] = grid[row][col] + min(move(row+1, col), move(row, col+1))
-
-            return memo[row][col]
-
-        return move(0,0)
-
-
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
 
         
+        return dp[0][0]
