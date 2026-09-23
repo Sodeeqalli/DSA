2c13b36d87e41ffc1370e35811ef066af96a7f9e
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-22 18:42:57 -0600

    Time: 7 ms (22.55%) | Memory: 19.3 MB (83.99%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,30 +1,54 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
         #my top down approach
+#         n = len(s)
+
+#         memo = [None] * (n+1)
+
+#         setDict = set(wordDict)
+#         print(setDict)
+
+#         def solve(i):
+#             print(i)
+#             if i == n:
+#                 return True
+#             if memo[i] != None:
+#                 return memo[i]
+
+
+#             for end in range(i, n):
+#                 if s[i:end+1] in setDict:
+#                     if solve(end+1):
+#                         memo[i] = True
+#                         return True
+
+#             memo[i] = False
+#             return False
+
+#         return solve(0)
+
+
+#bottom up
         n = len(s)
 
-        memo = [None] * (n+1)
-
+        dp = [0] * (n+1)
         setDict = set(wordDict)
-        print(setDict)
 
-        def solve(i):
-            print(i)
-            if i == n:
-                return True
-            if memo[i] != None:
-                return memo[i]
+        dp[n] = True
 
-
-            for end in range(i, n):
+        for i in range(n-1, -1, -1):
+            canBreak = False
+            for end in range(i,n):
                 if s[i:end+1] in setDict:
-                    if solve(end+1):
-                        memo[i] = True
-                        return True
+                    if dp[end+1] == True:
+                        canBreak = True
+                        break
+    
+            dp[i] = canBreak
 
-            memo[i] = False
-            return False
+        return dp[0]
+                
+    
 
-        return solve(0)
-
+        
         
