0a26a51d8beb50ac88adf900cbe58ed92158a827
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-23 10:01:11 -0600

    Time: 3 ms (61.40%) | Memory: 19.5 MB (19.34%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,21 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        #cost  = curCost + min(step+1, step+2)
-        #min cost from the last step is the cost of that step
-        #min cost for the second to last is the the cost of that step and the cost of the last cause it cannot take 2 steps
-
         n = len(cost)
-        minCost = [0] * n
-        minCost[n-1] = cost[n-1]
-        minCost[n-2] = cost[n-2]
 
+        twoAhead = 0
+        oneAhead = cost[-1]
 
-        for i in range(n-3, -1, -1):
-            minCost[i] = cost[i] + min(minCost[i+1], minCost[i+2])
+        if n == 1:
+            return oneAhead
 
+        for i in range(n-2, -1, -1):
+            cur = cost[i] + min(oneAhead, twoAhead)
 
+            twoAhead = oneAhead
+            oneAhead = cur
 
+        return min(oneAhead, twoAhead)
 
-        return min(minCost[0], minCost[1])
 
         
