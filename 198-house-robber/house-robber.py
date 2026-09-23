3b5331971e66e46ca9185ce22b16e92d9b5ba1d5
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-23 09:43:23 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.25%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,29 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
         n = len(nums)
+
+        dp2 = nums[-1]
         if n == 1:
-            return nums[0]
-        dp = [0] * n
-        dp[n-1] = nums[n-1]
-        dp[n-2] = max(nums[n-2], nums[n-1])
-
-        for i in range(n-3, -1, -1):
-            skip = dp[i+1]
-            rob = nums[i] + dp[i+2]
-
-            dp[i] = max(rob, skip)
+            return dp2
+        dp1 = max(nums[-2], nums[-1])
+        if n == 2:
+            return dp1
         
-        return dp[0]
+        for i in range(n-3, -1, -1):
+            cur = max(nums[i]+dp2, dp1)
+
+            dp2 = dp1
+            dp1 = cur
+
+        return cur
+
+
+        
+
+
+
+
+
+
+
         
