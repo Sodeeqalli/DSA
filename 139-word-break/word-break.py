a65eea5dae467e4c075ac83ffe087e78e36191a9
Alli Sodeeq Ayobami <sodeeqalli@gmail.com>
2026-09-22 18:25:43 -0600

    Time: 7 ms (22.55%) | Memory: 19.6 MB (9.64%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,23 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
-        #this very tricky to me
-        #i am not quite sure what the state is, maybe we start from the actual first letter
-        #i think i get it now, we can access future states so for current state we just check for every other index, when we see a word in the dictionary we start from the next letter
+        #my top down approach
         n = len(s)
+
+        memo = [None] * (n+1)
+
         setDict = set(wordDict)
-        memo = [None] * n
-        def breakWord(i):
+        print(setDict)
+
+        def solve(i):
+            print(i)
             if i == n:
                 return True
             if memo[i] != None:
                 return memo[i]
-            
-            for end in range(i,n):
+
+
+            for end in range(i, n):
                 if s[i:end+1] in setDict:
-                    if breakWord(end+1):
+                    if solve(end+1):
                         memo[i] = True
                         return True
+
             memo[i] = False
             return False
+
+        return solve(0)
+
         
-        return breakWord(0)
